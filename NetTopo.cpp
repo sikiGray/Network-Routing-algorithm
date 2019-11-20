56cc8b0141aa7783eaabfdc4579ad26edb41ebb7
zhao <2963286036@qq.com>
2019-11-21 00:33:17 +0800

    finish function NetTopo()

--- NetTopo.cpp
+++ NetTopo.cpp
@@ -1,1 +1,20 @@
-
+#include<iostream>
+using namespace std;
+const int DefaultVertices=200;  //默认的拓扑的最大顶点数
+const int inf=1e7;
+NetTopo::NetTopo(int int sz=DefaultVertices)
+{
+    maxVertices=sz;
+    numVertices=0;
+    numEdges=0;
+    VerticesList=new int[maxVertices];//建立顶点表数组
+    Edge=(int **)new int*[maxVertices];//建立邻接矩阵
+    for(int i=0;i<maxVertices;++i)
+        Edge[i]=new int[maxVertices];
+    //邻接矩阵初始化
+    for(int i=0;i<maxVertices;++i)
+        for(int j=0;j<maxVertices;++j)
+    {
+        Edge[i][j]=(i==j)? 0:inf;
+    }
+}
--- NetTopo.h
+++ NetTopo.h
@@ -1,18 +1,20 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+const int DefaultVertices=200;  //默认的拓扑的最大顶点数
 class NetTopo
 {
 public:
-    NetTopo(){}
+    NetTopo(int sz=DefaultVertices);
     //void createTopo();//建立拓扑图
     bool insertVertex(int v);//插入顶点
     bool insertEdge(int v1,int v2,int cost);//插入边(v1,v2),权值为cost
     bool removeVertex(int v);//删除顶点v及相关的边
     bool removeEdge(int v1,int v2);//删除边(v1,v2)
 private:
+    int *VerticesList;//顶点表
+    int **Edge;//邻接矩阵
     int maxVertices;//拓扑中最大顶点数
     int numEdges;//当前拓扑中的边数
     int numVertices;//当前拓扑中中的顶点数
-    vector<vector<int> > G;
 };
